2448e37b313f7578f083689e45a13e5ff3748f8b
Skelptin <mpainkra7@gmail.com>
2024-12-02 10:26:37 -0800

    added description component

--- frontend/src/components/Steps.jsx
+++ frontend/src/components/Steps.jsx
@@ -3,22 +3,24 @@ import { stepsData } from '../assets/assets'
 
 const Steps = () => {
     return (
-        <div>
+        <div className='flex flex-col items-center justify-center my-32'>
             <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>How it works</h1>
             <p className='text-lg text-gray-600 mb-8'>
                 Transform Words Into Stunning Images
             </p>
 
-            <div>
+            <div className='space-y-4 w-full max-w-3xl text-sm  '>
                 {
                     stepsData && stepsData.map((item, index) => (
-                        <div>
-                            <img src={item.icon} alt="" />
+                        <div key={index}
+                            className='flex items-center gap-4 p-5 px-8 bg-white/20
+                        shadow-md border rounded-lg cursor-pointer hover:scale-[1.02] transition-all duraiton-300'>
+                            <img w={40} src={item.icon} alt="" />
                             <div>
-                                <h2>
+                                <h2 className='etxt-xl font-medium'>
                                     {item.title}
                                 </h2>
-                                <p>{item.description}</p>
+                                <p className='text-gray-500'>"{item.description}"</p>
                             </div>
                         </div>
                     ))
